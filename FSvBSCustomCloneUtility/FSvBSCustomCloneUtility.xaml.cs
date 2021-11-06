ab08e23a4cc170e1d00ef09ec174ed682831c22c
Exkywor <fernandoquant.dev@gmail.com>
2021-11-05 21:06:31 -0600

    Add starting base skeleton

---
+++ FSvBSCustomCloneUtility/FSvBSCustomCloneUtility.xaml.cs
@@ -0,0 +1,48 @@
+﻿using LegendaryExplorerCore;
+using LegendaryExplorerCore.Packages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FSvBSCustomCloneUtility
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            initCoreLib();
+        }
+
+        /// <summary>
+        /// Initialize Legendary Explorer Core Library
+        /// </summary>
+        private static void initCoreLib()
+        {
+            static void packageSaveFailed(string message)
+            {
+                // I'm not sure if this requires ui thread since it's win32 but i'll just make sure
+                Application.Current.Dispatcher.Invoke(() =>
+                {
+                    MessageBox.Show(message);
+                });
+            }
+
+            LegendaryExplorerCoreLib.InitLib(TaskScheduler.FromCurrentSynchronizationContext(), packageSaveFailed);
+        }
+    }
+}
---
+++ FSvBSCustomCloneUtility/Tools/MorphWriter.cs
@@ -0,0 +1,47 @@
+﻿using LegendaryExplorerCore;
+using LegendaryExplorerCore.Packages;
+using MassEffectModManagerCore.modmanager.save.game3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSvBSCustomCloneUtility.Tools
+{
+    public class MorphWriter
+    {
+        private string ronFile;
+        private string targetFile;
+        private MorphHead morphHead;
+
+        public MorphWriter()
+        {
+        }
+
+        private void LoadCommands()
+        {
+
+        }
+
+        public void ApplyMorph(string ronFile, string targetFile)
+        {
+            LoadPCC(targetFile);
+            LoadMorph(ronFile);
+        }
+
+        private void LoadPCC(string targetFile)
+        {
+            // need to run some validation checks on the file first
+            this.targetFile = targetFile;
+            // TODO: Add check for ME3 or LE3 file
+            using IMEPackage pcc = MEPackageHandler.OpenMEPackage(targetFile);
+        }
+
+        private void LoadMorph(string ronFile)
+        {
+            // need to run some validation checks on the file first
+            morphHead = RONConverter.ConvertRON(ronFile);
+        }
+    }
+}
