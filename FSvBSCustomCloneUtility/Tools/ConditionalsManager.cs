21ecf0b3e7bd05347c898250e8607b6a0ccdc56e
Exkywor <fernandoquant.dev@gmail.com>
2021-11-24 11:54:09 -0600

    Add ConditionalsManager with template code

---
+++ FSvBSCustomCloneUtility/Tools/ConditionalsManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FSvBSCustomCloneUtility.Tools
+{
+    public static class ConditionalsManager
+    {
+        private static string path;
+
+        public static bool SetConditional(Gender gender, bool state, string path)
+        {
+            ConditionalsManager.path = path;
+            return false;
+        }
+
+
+        public static bool CheckConditional(Gender gender)
+        {
+            return false;
+        }
+
+        private static bool AddConditional(Gender gender)
+        {
+            return false;
+        }
+
+        private static bool RemoveConditional(Gender gender)
+        {
+            return false;
+        }
+    }
+
+}
