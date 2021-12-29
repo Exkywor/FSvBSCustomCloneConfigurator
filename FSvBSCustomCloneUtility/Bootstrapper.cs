ee6d07920f8bafe4536ff91b78d35403a5f4f631
Exkywor <fernandoquant.dev@gmail.com>
2021-12-29 11:54:27 -0600

    Add handling of uncaught exceptions.

--- FSvBSCustomCloneUtility/Bootstrapper.cs
+++ FSvBSCustomCloneUtility/Bootstrapper.cs
@@ -2,10 +2,12 @@
 using FSvBSCustomCloneUtility.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace FSvBSCustomCloneUtility
 {
@@ -19,6 +21,11 @@ namespace FSvBSCustomCloneUtility
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<MainWindowViewModel>();
+        }
+
+        protected override void OnUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e) {
+            MessageBox.Show(e.Exception.ToString(), "Error", MessageBoxButton.OK);;
+            e.Handled = true;
         }
     }
 }
