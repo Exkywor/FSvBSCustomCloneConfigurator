45615dde5072aee699307b908dffeb9e41c7aeee
Exkywor <fernandoquant.dev@gmail.com>
2022-01-10 15:57:37 -0600

    Finish Info window

--- FSvBSCustomCloneUtility/ViewModels/InfoWindowViewModel.cs
+++ FSvBSCustomCloneUtility/ViewModels/InfoWindowViewModel.cs
@@ -15,9 +15,41 @@ namespace FSvBSCustomCloneUtility.ViewModels {
             FileVersion = $"Version {FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location).FileVersion}";
         }
 
-        public void OpenGitHub() {
+        public void OpenSite(string target) {
+            string url = "";
+
+            switch (target) {
+                case "GH":
+                    url = "https://github.com/Exkywor/FSvBSCustomCloneUtility/";
+                    break;
+                case "ME3":
+                    url = "https://www.nexusmods.com/masseffect3/mods/975";
+                    break;
+                case "LE3":
+                    url = "https://www.nexusmods.com/masseffectlegendaryedition/mods/850";
+                    break;
+                case "LEX":
+                    url = "https://github.com/ME3Tweaks/LegendaryExplorer/blob/Beta/LegendaryExplorer/LegendaryExplorerCore/";
+                    break;
+                case "M3":
+                    url = "https://github.com/ME3Tweaks/ME3TweaksModManager/tree/master/MassEffectModManagerCore";
+                    break;
+                case "CM":
+                    url = "https://caliburnmicro.com/";
+                    break;
+                case "FA":
+                    url = "https://fontawesome.com/";
+                    break;
+                case "AU":
+                    url = "https://benruehl.github.io/adonis-ui/";
+                    break;
+                case "MW":
+                    url = "https://www.nuget.org/packages/Microsoft-WindowsAPICodePack-Shell/";
+                    break;
+            }
+
             Process.Start(new ProcessStartInfo {
-                FileName = $"https://github.com/Exkywor/FSvBSCustomCloneUtility/",
+                FileName = url,
                 UseShellExecute = true
             });
         }
